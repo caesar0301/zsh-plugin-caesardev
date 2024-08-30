3cecf866a9303871817d13132577ccf579023f35
陈夏明 <chenxm35@gmail.com>
2024-08-30 14:04:07 +0800

    Update caesardev.plugin.zsh

--- caesardev.plugin.zsh
+++ caesardev.plugin.zsh
@@ -51,6 +51,50 @@ alias ag_bazel='ag --ignore-dir="build" -G "(BUILD|.\.bazel)"'
 # functions
 #+++++++++++++++++++++++++++++++++++++++
 
+# Overhead configrator for apps
+function occ {
+    subcommand=$1;
+    if [ ! -z $subcommand ]; then
+        shift
+    fi
+    case "$subcommand" in
+        emacs|em) cf=$HOME/.emacs.d/init.el
+        ;;
+        vi|vim|nvim) cf=$HOME/.config/nvim/init.lua
+        ;;
+        zsh) cf=${ZSH_CONFIG_DIR}/init.zsh
+            while getopts eh opt; do
+                case $opt in
+                    e)    cf=$HOME/.zshenv ;;
+                    h|?)  echo "occ zsh [-e]" && return;;
+                esac
+            done
+        ;;
+        ssh) cf=$HOME/.ssh/config
+        ;;
+        tmux) cf=$HOME/.config/tmux/tmux.conf.local
+        ;;
+        clash) cf=$HOME/.config/clash/config.yaml
+        ;;
+        proxychains|pc) cf=/etc/proxychains.conf
+        ;;
+        yum) cf=/etc/yum.conf
+        ;;
+        apt) cf=/etc/apt/sources.list
+        ;;
+        conan) cf=$HOME/.conan2/profiles/default
+        ;;
+        * ) # Invalid subcommand
+            if [ ! -z $subcommand ]; then  # Don't show if no subcommand provided
+                echo "Invalid subcommand: $subcommand"
+            fi
+            echo "Usage: occ <emacs|em|vim|vi|zsh|ssh|tmux|clash|pc|yum|apt|conan>"
+            return
+        ;;
+    esac
+    ${=EDITOR} $cf
+}
+
 function _flatpak_aliases {
     FLATPAK_HOME=/var/lib/flatpak
     FLATPAK_BIN=${FLATPAK_HOME}/exports/bin
